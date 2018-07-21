64fe1099a99d9580f9e709ce1b0b8c3319bf6428
Raseshwari <rp3737@rit.edu>
2018-07-21 19:20:00 -0400

    Test cases for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} =  require('./../models/todos');
 
+const todos = [{
+	text : 'Sample todo 1'
+}, {
+	text: 'Sample todo 2'
+}];
+
 //testing lifecycle method to remove all changes made in db
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() =>{
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 
@@ -25,7 +33,7 @@ describe('POST/todos', () =>{
 				return done(err);
 			}
 
-			Todo.find().then((todos) => {
+			Todo.find({text}).then((todos) => {
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).toBe(text);
 			done();
@@ -38,15 +46,28 @@ describe('POST/todos', () =>{
 		.post('/todos')
 		.send({})
 		.expect(400)
-		.end((err,res) => {
+		.end((err,res) => { //passing dollowing code to end() because checking db asynchronously
 			if(err){
 				return done(err);
 			}
 
 			Todo.find().then((todos) => {
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			}).catch((e) => done(e));
 		});
 	});
  });
+
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.todos.length).toBe(2);
+		})
+		.end(done);
+	})
+});
