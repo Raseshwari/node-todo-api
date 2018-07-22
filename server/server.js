d1a525cb3175e217f466b34f7cc9a74e55bef9f4
Raseshwari <rp3737@rit.edu>
2018-07-21 20:31:59 -0400

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} =  require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
 var {User} = require('./models/user');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) =>{
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) =>{
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		res.status(404).send('Invalid id!');
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			res.status(404).send('Todo with that id not found');
+		}
+		res.status(200).send({todo});
+	}, (e)=>{
+		res.status(404).send('');
 	});
 });
 
